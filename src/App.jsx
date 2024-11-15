289177c853c73956507424eb08b56048ce95047e
Vincenzo-Polverino <94polverino.vincenzo@gmail.com>
2024-11-15 16:47:20 +0100

    create function

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,21 @@ import languages from './languages'
 import './App.css'
 
 function App() {
+  const [selectLang, setSelectLang] = useState(languages[0])
 
 
   return (
     <>
+      <div className='button'>
+        {languages.map(language => (<button key={language.id} onClick={() => setSelectLang(language)}>
+          {language.title}
+        </button>
+        ))}
+      </div>
+      <div className='card'>
+        <h2>{selectLang.title}</h2>
+        <p>{selectLang.description}</p>
+      </div>
 
     </>
   )
